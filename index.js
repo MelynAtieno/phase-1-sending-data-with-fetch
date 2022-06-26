17cd54b5a06f4a6cb20469bb018b7bfa37ccbfaf
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-26 18:42:13 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData( UserName, EmailAddress) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          UserName,
+          EmailAddress
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+ 
